1d0a0a41665c330d314d17fc72f31b1700c8b0b3
Soroush Ahrari <ahrari.soroush76@gmail.com>
2023-07-07 00:04:49 +0200

    add redis service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PromptModule } from './modules/prompt/prompt.module';
+import { RedisModule } from './modules/redis/redis.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [PromptModule],
+    imports: [
+        PromptModule,
+        RedisModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+    ],
     controllers: [],
     providers: [],
 })
---
+++ src/modules/redis/services/redis.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisService {
+    private readonly _client: any;
+    constructor(private configService: ConfigService) {
+        this._client = new Redis({
+            host: this.configService.get<string>('REDIS_HOST'),
+            port: this.configService.get<number>('REDIS_PORT'),
+        });
+    }
+
+    get client(): any {
+        return this._client;
+    }
+
+    async get(key: string): Promise<string> {
+        return await this.client.get(key);
+    }
+
+    async set(key: string, value: string): Promise<void> {
+        await this.client.set(key, value);
+    }
+
+    async del(key: string): Promise<void> {
+        await this.client.del(key);
+    }
+
+    async keys(pattern: string): Promise<string[]> {
+        return await this.client.keys(pattern);
+    }
+
+    async values(pattern: string): Promise<string[]> {
+        const values: string[] = [];
+        let cursor = '0';
+
+        do {
+            const res = await this.client.scan(cursor, 'MATCH', pattern);
+            cursor = res[0];
+            const keys: [] = res[1];
+
+            if (keys.length > 0) {
+                const retrievedValues = await this.client.mget(...keys);
+                values.push(...retrievedValues);
+            }
+        } while (cursor !== '0');
+
+        return values;
+    }
+
+    async flushall(): Promise<void> {
+        await this.client.flushall();
+    }
+}
