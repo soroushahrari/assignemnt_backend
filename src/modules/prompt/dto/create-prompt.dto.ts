c9c211fcb0bd4cca3c5980b382696d5c1a05938a
Soroush Ahrari <ahrari.soroush76@gmail.com>
2023-07-07 15:28:49 +0200

    add prompt dtos

--- src/modules/prompt/dto/create-prompt.dto.ts
+++ src/modules/prompt/dto/create-prompt.dto.ts
@@ -1,4 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreatePromptDto {
+    @ApiProperty({
+        required: true,
+        description: 'The title of the prompt',
+        example: 'My Prompt',
+        type: String,
+    })
+    @IsNotEmpty()
+    @IsString()
     title: string;
+
+    @ApiProperty({
+        required: true,
+        description: 'The description of the prompt',
+        example: 'My Prompt description',
+        type: String,
+    })
+    @IsNotEmpty()
+    @IsString()
     description: string;
 }
--- src/modules/prompt/dto/update-prompt.dto.ts
+++ src/modules/prompt/dto/update-prompt.dto.ts
@@ -1,8 +1,35 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreatePromptDto } from './create-prompt.dto';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class UpdatePromptDto extends PartialType(CreatePromptDto) {
+    @ApiProperty({
+        required: false,
+        description: 'The title of the prompt',
+        example: 'My Prompt',
+        type: String,
+    })
+    @IsOptional()
+    @IsString()
     title?: string;
+
+    @ApiProperty({
+        required: false,
+        description: 'The description of the prompt',
+        example: 'My Prompt description',
+        type: String,
+    })
+    @IsOptional()
+    @IsString()
     description?: string;
+
+    @ApiProperty({
+        required: false,
+        description: 'Whether the prompt is a favorite',
+        example: true,
+        type: Boolean,
+    })
+    @IsOptional()
+    @IsBoolean()
     favorite?: boolean;
 }
