2a1013e3e7cf488dfd413cd1efdc9dbc7dce8df9
Soroush Ahrari <ahrari.soroush76@gmail.com>
2023-07-07 15:30:21 +0200

    add swagger documents

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from 'http-exception.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalFilters(new HttpExceptionFilter());
+    const config = new DocumentBuilder()
+        .setTitle('Prompt manager API')
+        .setDescription('The prompt manager API description')
+        .setVersion('1.0')
+        .addTag('prompt')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
