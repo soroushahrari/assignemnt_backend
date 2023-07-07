ef846d62a4026d8e043a107979dd555a79b5d2a8
Soroush Ahrari <ahrari.soroush76@gmail.com>
2023-07-07 15:29:03 +0200

    add prompt service methods

--- src/modules/prompt/services/prompt.service.ts
+++ src/modules/prompt/services/prompt.service.ts
@@ -1,26 +1,105 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePromptDto } from '../dto/create-prompt.dto';
 import { UpdatePromptDto } from '../dto/update-prompt.dto';
-
+import { Prompt } from '../entities/prompt.entity';
+import { v4 as uuidv4 } from 'uuid';
+import { RedisService } from 'src/modules/redis/services/redis.service';
 @Injectable()
 export class PromptService {
-    create(createPromptDto: CreatePromptDto) {
-        return 'This action adds a new prompt';
+    constructor(private redisService: RedisService) {}
+
+    async create(createPromptDto: CreatePromptDto): Promise<Prompt> {
+        const { title, description } = createPromptDto;
+
+        const newpRompt: Prompt = {
+            id: uuidv4(),
+            title,
+            description,
+            favorite: false,
+            createdAt: new Date(),
+            lastModifiedAt: null,
+        };
+
+        const response = await this.redisService.set(
+            newpRompt.id,
+            JSON.stringify(newpRompt),
+        );
+
+        if (response != 'OK') {
+            throw new Error('Error while creating prompt');
+        }
+
+        return newpRompt;
     }
 
-    findAll() {
-        return `This action returns all prompt`;
+    async findAll(): Promise<Prompt[]> {
+        const prompts = await this.redisService.values('*');
+
+        return prompts.map((prompt) => {
+            return JSON.parse(prompt);
+        });
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} prompt`;
+    async findOne(id: string): Promise<Prompt> {
+        const prompt = await this.redisService.get(id);
+
+        if (!prompt) {
+            throw new Error('Prompt not found');
+        }
+
+        return JSON.parse(prompt);
     }
 
-    update(id: number, updatePromptDto: UpdatePromptDto) {
-        return `This action updates a #${id} prompt`;
+    async update(
+        id: string,
+        updatePromptDto: UpdatePromptDto,
+    ): Promise<Prompt> {
+        let prompt;
+        try {
+            prompt = await this.redisService.get(id);
+        } catch (error) {
+            throw error;
+        }
+
+        if (!prompt) {
+            throw new Error('Prompt not found');
+        }
+
+        const updatedPrompt: Prompt = {
+            ...JSON.parse(prompt),
+            ...updatePromptDto,
+            lastModifiedAt: new Date(),
+        };
+
+        const response = await this.redisService.set(
+            id,
+            JSON.stringify(updatedPrompt),
+        );
+
+        if (response != 'OK') {
+            throw new Error('Error while updating prompt');
+        }
+
+        return updatedPrompt;
     }
 
-    remove(id: number) {
-        return `This action removes a #${id} prompt`;
+    async remove(id: string): Promise<boolean> {
+        const response = await this.redisService.del(id);
+
+        if (response == 0) {
+            throw new Error('Prompt not found');
+        }
+
+        return true;
+    }
+
+    async removeAll(): Promise<boolean> {
+        const response = await this.redisService.flushall();
+
+        if (response != 'OK') {
+            throw new Error('Error while deleting all prompts');
+        }
+
+        return true;
     }
 }
