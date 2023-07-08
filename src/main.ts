430cc47bcb4c7d9c218fc1069718e8db09ac3ac2
Soroush Ahrari <ahrari.soroush76@gmail.com>
2023-07-08 03:53:43 +0200

    add validation pipes

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors();
     app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalPipes(new ValidationPipe());
     const config = new DocumentBuilder()
         .setTitle('Prompt manager API')
         .setDescription('The prompt manager API description')
