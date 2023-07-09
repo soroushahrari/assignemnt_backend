4cb6cf32ff7c762b87c462fedc201b33ea203793
Soroush Ahrari <ahrari.soroush76@gmail.com>
2023-07-09 16:25:47 +0200

    add user service functionality

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,23 +1,67 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { RedisService } from '../redis/services/redis.service';
+import { User } from './entities/user.entity';
+import { v4 as uuidv4 } from 'uuid';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
-    constructor() {}
+    constructor(
+        private redisService: RedisService,
+        private configService: ConfigService,
+        ) { }
 
     async create(createUserDto: CreateUserDto) {
-        return createUserDto;
+
+        const newUser: User = {
+            id: uuidv4(),
+            createdAt: new Date(),
+            ...createUserDto,
+        };
+
+        const userKey = `${this.configService.get<string>('USER_PREFIX')}${
+            newUser.id
+        }`;
+        const response = await this.redisService.set(
+            userKey,
+            JSON.stringify(newUser),
+        );
+
+        if (response !== 'OK') {
+            throw new Error('Error while creating user');
+        }
+
+        return newUser;
     }
 
-    async findAll() {
-        return 'This action returns all user';
+    async findAll(): Promise<User[]> {
+        const users = await this.redisService.values('user:*');
+
+        return users.map((user) => {
+            return JSON.parse(user);
+        });
     }
 
-    async findById(id: string) {
-        return `This action returns a #${id} user`;
+    async findById(id: string): Promise<User> {
+        const user = await this.redisService.get(`user:${id}`);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        return JSON.parse(user);
     }
 
-    async findByEmail(email: string) {
-        return `This action returns a ${email} user`;
+    async findByEmail(email: string): Promise<User> {
+        const users = await this.findAll();
+
+        const user = users.find((user) => user.email === email);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        return user;
     }
 }
